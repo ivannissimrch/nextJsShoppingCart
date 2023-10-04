54fc68c5981dc4ed2c11742f8154a04017b07ee4
ivannissimrch <ivannissimrch@gmail.com>
2023-10-03 22:41:01 -0400

    Add Navbar

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <Link href="/">Home</Link>
+          <Link href="/products">Products</Link>
+          <Link href="/cart">Cart</Link>
+        </ul>
+      </nav>
+    </header>
+  );
+}
