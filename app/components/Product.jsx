1f7abb66ad6e184770bbbf491de72ad6bcd96744
ivannissimrch <ivannissimrch@gmail.com>
2023-10-08 17:04:44 -0400

    Add temporary global css styles

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -1,3 +1,4 @@
+"use client";
 import Card from "./Card";
 
 async function getProducts(id) {
@@ -9,12 +10,17 @@ async function getProducts(id) {
   }
 }
 
+// eslint-disable-next-line @next/next/no-async-client-component
 export default async function Product({ id }) {
   const product = await getProducts(id);
+  const handleClick = () => {
+    console.log(product);
+  };
   return (
     <div>
       <Card productData={product} key={product.id} />
       <p>{product.description}</p>
+      <button onClick={handleClick}>Add to Cart</button>
     </div>
   );
 }
--- app/products/[productId]/page.jsx
+++ app/products/[productId]/page.jsx
@@ -2,6 +2,9 @@ import Product from "@/app/components/Product";
 
 export default function ProductDetails({ params }) {
   const { productId } = params;
+  const handleClick = () => {
+    alert(productId);
+  };
   return (
     <main>
       <Product id={productId} />
