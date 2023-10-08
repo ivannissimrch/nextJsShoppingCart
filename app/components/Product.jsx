35f9f39b787dc9f226f7b4b4f350f0bf27129778
ivannissimrch <ivannissimrch@gmail.com>
2023-10-08 19:42:54 -0400

    remove use client from Product component

--- app/components/Product.jsx
+++ app/components/Product.jsx
@@ -1,4 +1,4 @@
-"use client";
+// "use client";
 import Card from "./Card";
 
 async function getProducts(id) {
@@ -13,14 +13,15 @@ async function getProducts(id) {
 // eslint-disable-next-line @next/next/no-async-client-component
 export default async function Product({ id }) {
   const product = await getProducts(id);
-  const handleClick = () => {
-    console.log(product);
-  };
+  // const handleClick = () => {
+  //Add product to cart
+  //navigate to cart
+  // };
   return (
     <div>
       <Card productData={product} key={product.id} />
       <p>{product.description}</p>
-      <button onClick={handleClick}>Add to Cart</button>
+      {/* <button onClick={handleClick}>Add to Cart</button> */}
     </div>
   );
 }
--- app/products/[productId]/page.jsx
+++ app/products/[productId]/page.jsx
@@ -2,9 +2,6 @@ import Product from "@/app/components/Product";
 
 export default function ProductDetails({ params }) {
   const { productId } = params;
-  const handleClick = () => {
-    alert(productId);
-  };
   return (
     <main>
       <Product id={productId} />
